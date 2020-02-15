ae932dea80b7f6a950901178f7d318f2b299bf32
vaso369 <vasilije.vasilijevic.11.17@ict.edu.rs>
2020-02-15 14:43:50 +0100

    initial commit

---
+++ consts/consts.js
@@ -0,0 +1,12 @@
+//import { React } from "react";
+// import { useStateGlobal } from "../src/GlobalState";
+// const state = useStateGlobal();
+
+export const url = "http://localhost/task-management-sys-v1/backend/index.php";
+
+export const picturePath =
+  "http://localhost/task-management-sys-v1/backend/app/";
+
+export const urlRedirect = "http://localhost:3000";
+
+export const defaultPicture = `http://localhost/task-management-sys-v1/backend/app/assets/avatar-placeholder.png`;
