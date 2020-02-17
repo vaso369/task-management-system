4958682df10635f94de24803668057b36f9b67f4
vaso369 <59292985+vaso369@users.noreply.github.com>
2020-02-17 13:27:06 +0100

    Update urls

--- consts/consts.js
+++ consts/consts.js
@@ -2,11 +2,11 @@
 // import { useStateGlobal } from "../src/GlobalState";
 // const state = useStateGlobal();
 
-export const url = "http://localhost/task-management-sys-v1/backend/index.php";
+export const url = "https://task-sys-api.000webhostapp.com/backend/index.php";
 
 export const picturePath =
-  "http://localhost/task-management-sys-v1/backend/app/";
+  "https://task-sys-api.000webhostapp.com/backend/app/";
 
-export const urlRedirect = "http://localhost:3000";
+export const urlRedirect = "https://task-sys-api.000webhostapp.com";
 
-export const defaultPicture = `http://localhost/task-management-sys-v1/backend/app/assets/avatar-placeholder.png`;
+export const defaultPicture = `https://task-sys-api.000webhostapp.com/backend/app/assets/avatar-placeholder.png`;
