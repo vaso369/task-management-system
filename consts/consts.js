d05c71bc1a5b2f32befb850d0589ac63de3cfbf2
vaso369 <59292985+vaso369@users.noreply.github.com>
2020-02-18 12:24:13 +0100

    Update consts.js

--- consts/consts.js
+++ consts/consts.js
@@ -2,7 +2,7 @@
 // import { useStateGlobal } from "../src/GlobalState";
 // const state = useStateGlobal();
 
-export const url = "https://task-sys-api.000webhostapp.com/index.php";
+export const url = "https://task-sys-backend.herokuapp.com/index.php";
 
 export const picturePath =
   "https://task-sys-api.000webhostapp.com/backend/app/";
