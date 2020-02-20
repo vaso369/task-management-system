c2cc2e25b730942b1cbb26b434a6c9828293e560
vaso369 <vasilije.vasilijevic.11.17@ict.edu.rs>
2020-02-20 20:58:03 +0100

    changes

--- pages/index.js
+++ pages/index.js
@@ -18,10 +18,31 @@ const Index = () => {
         <Logo />
         <MessageAuth />
       </div>
+      <div
+        id="dokumentacija"
+        style={{
+          width: "20%",
+          position: "absolute",
+          bottom: "2%",
+          right: "12%",
+          color: "white"
+        }}
+      >
+        <a href="https://api-task-management.000webhostapp.com/backend/Documentacija.pdf">
+          Documentation
+        </a>
+      </div>
       <style global jsx>{`
         * {
           margin: 0;
           padding: 0;
+        }
+        ,
+        #dokumentacija {
+          width: 30%;
+          position: absolute;
+          bottom: 3%;
+          right: 25%;
         }
       `}</style>
     </React.Fragment>
